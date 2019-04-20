a740cb72186be844476b37099d8539850e1de13a
Juho Keski-Rahkonen <juho.keski-rahkonen@maplet.com>
2019-04-20 16:29:31 +0300

    Setting up for production

--- src/config.js
+++ src/config.js
@@ -1,2 +1,10 @@
 
-export const API_URL = "localhost:4000";
+let API_URL = "localhost:4000";
+
+if (process.env.NODE_ENV === 'production') {
+  API_URL = "catapi-telia.herokuapp.com";
+}
+
+export {
+  API_URL
+};
